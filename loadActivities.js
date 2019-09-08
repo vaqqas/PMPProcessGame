4e3e6686447e21513e48640a5f27f62a815271ed
pure-mvaq <md.vaqqas@puresoftware.com>
2019-09-08 13:21:14 +0530

    first upload

---
+++ loadActivities.js
@@ -0,0 +1,97 @@
+function loadActivities()
+{
+activities =[
+{name:"Select Project Manager",phase:1,seq:0}
+,{name:"Determine company culture and existing system",phase:1,seq:0}
+,{name:"Collect processes, procedures and historical information",phase:1,seq:0}
+,{name:"Divide large projects into phases or smaller projects",phase:1,seq:0}
+,{name:"Understand business case and benefits management plan",phase:1,seq:0}
+,{name:"Uncover initial requirements, assumptions, risks, constraints and existing agreements",phase:1,seq:0}
+,{name:"Assess project and product feasibility within the given constraints",phase:1,seq:0}
+,{name:"Create measurable objectives and success criteria",phase:1,seq:0}
+,{name:"Develop project charter",phase:1,seq:0}
+,{name:"Identify stakeholders and determine their expectations, interest, influence, and impact",phase:1,seq:0}
+,{name:"Request Changes-1",phase:1,seq:0}
+,{name:"Develop assumptions log",phase:1,seq:0}
+,{name:"Develop stakeholder register",phase:1,seq:0}
+,{name:"Determine development approach, lifecycle, and how you will plan for each knowledge area",phase:2,seq:1}
+,{name:"Define and prioritize requirements",phase:2,seq:2}
+,{name:"Create project scope statement",phase:2,seq:3}
+,{name:"Assess what to purchase and create procurement documents",phase:2,seq:4}
+,{name:"Determine Planning Team",phase:2,seq:5}
+,{name:"Create WBS and WBS Dictionary",phase:2,seq:6}
+,{name:"Create activity list",phase:2,seq:7}
+,{name:"Create Network Diagram",phase:2,seq:8}
+,{name:"Estimate resource requirements",phase:2,seq:9}
+,{name:"Estimate activity durations and costs",phase:2,seq:10}
+,{name:"Determine critical path",phase:2,seq:11}
+,{name:"Develop schedule",phase:2,seq:12}
+,{name:"Develop budget",phase:2,seq:13}
+,{name:"Determine quality standards, processes, and metrics",phase:2,seq:14}
+,{name:"Determine team charter and all roles and responsibilities",phase:2,seq:15}
+,{name:"Plan communications and stakeholder engagement",phase:2,seq:16}
+,{name:"Perform risk identification, qualitative and quantitative risk analysis, and risk response planning",phase:2,seq:17}
+,{name:"Go back - iterations",phase:2,seq:18}
+,{name:"Finalize procurement strategy and documents",phase:2,seq:19}
+,{name:"Create change and configuration management plans",phase:2,seq:20}
+,{name:"Finalize all management plans",phase:2,seq:21}
+,{name:"Develop realistic and sufficient project management plan and baseline",phase:2,seq:22}
+,{name:"Gain formal approval of the plan",phase:2,seq:23}
+,{name:"Hold kick-off meeting",phase:2,seq:24}
+,{name:"Request changes-2",phase:2,seq:25}
+,{name:"Execute work according to the project management plan",phase:3,seq:0}
+,{name:"Produce product deliverables (product scope)",phase:3,seq:0}
+,{name:"Gather work performance data",phase:3,seq:0}
+,{name:"Request changes",phase:3,seq:0}
+,{name:"Implement only approved changes",phase:3,seq:0}
+,{name:"Continuously improve; perform progressive elaboration",phase:3,seq:0}
+,{name:"Follow processes",phase:3,seq:0}
+,{name:"Determine whether quality plan and processes are correct and effective",phase:3,seq:0}
+,{name:"Performa quality audits and issue quality report",phase:3,seq:0}
+,{name:"Acquire final team and physical resources",phase:3,seq:0}
+,{name:"Manage people",phase:3,seq:0}
+,{name:"Evaluate team and individual performance; provide training",phase:3,seq:0}
+,{name:"Hold team-building activities",phase:3,seq:0}
+,{name:"Give recognition and rewards",phase:3,seq:0}
+,{name:"Use issue logs",phase:3,seq:0}
+,{name:"Facilitate conflict resolution",phase:3,seq:0}
+,{name:"Release resources as work is completed",phase:3,seq:0}
+,{name:"Send and receive information, and solicit feedback",phase:3,seq:0}
+,{name:"Report on project performance",phase:3,seq:0}
+,{name:"Facilitate stakeholder engagement and manage expectations",phase:3,seq:0}
+,{name:"Hold meetings",phase:3,seq:0}
+,{name:"Evaluate sellers; negotiate and contract with sellers",phase:3,seq:0}
+,{name:"Use and share project knowledge",phase:3,seq:0}
+,{name:"Execute contingency plans",phase:3,seq:0}
+,{name:"Update project management plan and project documents",phase:3,seq:0}
+,{name:"Take action to monitor and control the project",phase:4,seq:0}
+,{name:"Measure performance against performance measurement baseline",phase:4,seq:0}
+,{name:"Measure performance against other metrics in the project management plan",phase:4,seq:0}
+,{name:"Determine if variances warrant a corrective action or other change request(s)",phase:4,seq:0}
+,{name:"Influence factors that cause change",phase:4,seq:0}
+,{name:"Request changes - 4",phase:4,seq:0}
+,{name:"Performa integrated change control",phase:4,seq:0}
+,{name:"Approve or reject changes",phase:4,seq:0}
+,{name:"Update project management plan and project documents",phase:4,seq:0}
+,{name:"Inform stakeholders of all change request results",phase:4,seq:0}
+,{name:"Monitor stakeholder engagement",phase:4,seq:0}
+,{name:"Confirm configuration compliance",phase:4,seq:0}
+,{name:"Create forecasts",phase:4,seq:0}
+,{name:"Gain customer's acceptance of interim deliverables",phase:4,seq:0}
+,{name:"Perform quality control",phase:4,seq:0}
+,{name:"Perform risk control",phase:4,seq:0}
+,{name:"Perform risk reviews, reassessments, and audits",phase:4,seq:0}
+,{name:"Manage reserves",phase:4,seq:0}
+,{name:"Manage, evaluate, and close procurements",phase:4,seq:0}
+,{name:"Evaluate use of physical resources",phase:4,seq:0}
+,{name:"Confirm work is done to requirements",phase:5,seq:0}
+,{name:"Complete final procurement closure",phase:5,seq:0}
+,{name:"Gain final acceptance of product",phase:5,seq:0}
+,{name:"Complete financial closure",phase:5,seq:0}
+,{name:"Hand off completed product",phase:5,seq:0}
+,{name:"Solicit customer's feedback about the project",phase:5,seq:0}
+,{name:"Complete final performance reporting",phase:5,seq:0}
+,{name:"Index and archive records",phase:5,seq:0}
+,{name:"Gather final lessons learned and update knowledge bases",phase:5,seq:0}   
+];
+}
