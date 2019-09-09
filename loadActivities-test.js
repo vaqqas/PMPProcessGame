c94505c692e5744f574736f7d13e9e3881018cba
pure-mvaq <md.vaqqas@puresoftware.com>
2019-09-10 01:38:49 +0530

    Fixed scoring issues.

---
+++ loadActivities-test.js
@@ -0,0 +1,9 @@
+function loadActivities()
+{
+activities =[
+{name:"Select Project Manager",phase:1,seq:0}
+,{name:"Determine company culture and existing system",phase:1,seq:0}
+,{name:"Define and prioritize requirements",phase:2,seq:2}
+,{name:"Complete final procurement closure",phase:5,seq:0}
+];
+}
